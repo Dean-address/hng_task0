7d2ee211574992f3659d0cf6f506a28b00553b7a
Alabi Khalid <alabikhalid.12@gmail.com>
2025-01-31 10:44:41 +0100

    update

--- stage0/settings.py
+++ stage0/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DEBUG"]
+DEBUG = False
+# DEBUG = False
 
 ALLOWED_HOSTS = ast.literal_eval(os.environ["ALLOWED_HOSTS"])
 
